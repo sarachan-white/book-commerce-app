dac841aad1ff36f00de0d813ff90edaaaad29df2
sarachan-white <sarachan-white-toy-poodle@outlook.jp>
2024-01-01 23:19:44 +0900

    done

---
+++ app/api/purchases/[userId]/route.ts
@@ -0,0 +1,17 @@
+import prisma from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+//購入履歴検索API
+export async function GET(request: Request, {params}: {params: {userId: string}}) {
+    const userId = params.userId;
+
+    try {
+        const purchase = await prisma.purchase.findMany({
+            where: {userId: userId},
+        });
+
+        return NextResponse.json(purchase);
+    } catch (error) {
+        return NextResponse.json(error);
+    }
+}
